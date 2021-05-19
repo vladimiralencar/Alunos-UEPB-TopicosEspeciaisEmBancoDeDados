4943fcdabf877affa14979b07cf9319281cd2acb
vladimiralencar <valencar@gmail.com>
2021-05-18 21:17:49 -0300

    new

---
+++ BigData/sgi.py
@@ -0,0 +1,19 @@
+import PySimpleGUI as sg                        # Part 1 - The import
+
+# Define the window's contents
+layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
+            [sg.Input()],
+            [sg.Button('Ok')] ]
+
+# Create the window
+window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+
+# Display and interact with the Window
+event, values = window.read()                   # Part 4 - Event loop or Window.read call
+
+# Do something with the information gathered
+print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+
+# Finish up by removing from the screen
+window.close()   
+
